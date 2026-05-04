d427b2f02f34df52ee9b9614bbc9e55caa69372c
bkonsamb <bkonsamb@gmail.com>
2026-05-04 19:02:55 +0000

    Create categories.ts

---
+++ src/data/categories.ts
@@ -0,0 +1,35 @@
+export const CATEGORIES = [
+  "Tous les secteurs",
+  "Informatique & Tech",
+  "Marketing & Communication",
+  "Finance & Comptabilité",
+  "BTP & Génie Civil",
+  "Santé & Médical",
+  "Vente & Commerce",
+  "Juridique & Droit",
+  "Logistique & Transport",
+  "Éducation & Formation",
+  "Ressources Humaines",
+  "Agriculture & Environnement",
+];
+
+export const LOCATIONS = [
+  "Toutes les villes",
+  "Dakar",
+  "Thiès",
+  "Saint-Louis",
+  "Ziguinchor",
+  "Kaolack",
+  "Mbour",
+  "Rufisque",
+  "Touba",
+];
+
+export const CONTRACT_TYPES = [
+  "Tous les contrats",
+  "CDI",
+  "CDD",
+  "Stage",
+  "Freelance",
+  "Temps partiel",
+];
