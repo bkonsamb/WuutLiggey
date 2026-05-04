5febd6bdc9e2c5c09ac248483bcb338892fd2d62
bkonsamb <bkonsamb@gmail.com>
2026-05-04 19:15:19 +0000

    Create Job.ts

---
+++ src/types/Job.ts
@@ -0,0 +1,14 @@
+export interface Job {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+  type: string;
+  category: string;
+  salary: string;
+  date: string;
+  image: string;
+  excerpt: string;
+  description: string;
+  tags: string[];
+}
